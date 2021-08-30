c5c0a1d089e088ecb70644b70e36d0ce62b89430
Adriela08 <89789017+Adriela08@users.noreply.github.com>
2021-08-30 15:54:28 -0300

    primeiroPasso

---
+++ Test_Python_autom.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+navegador = webdriver.Chrome()
+navegador.get("https://ri.magazineluiza.com.br/")
+navegador.find_element_by_xpath('//*[@id="owl-destaques"]/div[1]/div/div[4]/div/a/p').click()
+
+navegador.find_element_by_xpath('//*[@id="bWtQ7n6RcQdDDDCgCcH3yg=="]').click()
